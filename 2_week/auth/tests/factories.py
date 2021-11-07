644d434c49524bf23c1fec2631052da8a7b6eb40
confar <tentigers@mail.ru>
2021-11-07 21:10:43 +0300

    add oauth+jwt

--- 2_week/auth/tests/factories.py
+++ 2_week/auth/tests/factories.py
@@ -2,7 +2,6 @@ import factory
 from sqlalchemy import orm
 
 DBLSession = orm.scoped_session(orm.sessionmaker())
-DBHSession = orm.scoped_session(orm.sessionmaker())
 
 
 class DBLBaseModelFactory(factory.alchemy.SQLAlchemyModelFactory):
@@ -11,9 +10,3 @@ class DBLBaseModelFactory(factory.alchemy.SQLAlchemyModelFactory):
         sqlalchemy_session_persistence = "flush"
         sqlalchemy_session = DBLSession
 
-
-class DBHBaseModelFactory(factory.alchemy.SQLAlchemyModelFactory):
-    class Meta:
-        abstract = True
-        sqlalchemy_session_persistence = "flush"
-        sqlalchemy_session = DBHSession
